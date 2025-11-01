9c724cadb7e02a95fb6951ecc8771d166b299f7f
klantle <222417345+klantle@users.noreply.github.com>
2025-11-01 21:25:48 +0700

    improved

--- run-native.bat
+++ run-native.bat
@@ -1,4 +1,24 @@
 @echo off
 cd /d %~dp0
 set PATH=C:\libwatchdogs;%PATH%
-watchdogs.win
+
+start "" watchdogs.win
+
+:wait_start
+tasklist /fi "imagename eq watchdogs.win" | find "watchdogs.win" >nul
+if %errorlevel% neq 0 (
+    timeout /t 1 /nobreak >nul
+    goto :wait_start
+)
+
+echo .. Watchdogs has been started. This is the Command Prompt terminal that will appear if Watchdogs stops running...
+
+:monitor
+timeout /t 1 /nobreak >nul
+tasklist /fi "imagename eq watchdogs.win" | find "watchdogs.win" >nul
+if %errorlevel%==0 (
+    goto :monitor
+) else (
+    echo Program ended. Opening CMD...
+    start cmd /k "cd /d %CD%"
+)
